4ecf4fa9a2c48aec900df98f52d8ab429159a41c
Kenny Wong <wky123@hotmail.co.uk>
2019-09-14 22:53:31 +0100

    build runner

--- runner.py
+++ runner.py
@@ -3,3 +3,15 @@ from transformer import Transformer
 
 import tensorflow as tf
 
+
+def train_step(model, loss_obj, optimizer, inputs, targets):
+    with tf.GradientTape as tape:
+        preds = model(inputs)
+        loss_val = loss_obj(targets, preds)
+    gradients = tape.gradient(loss_val, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    
+
+def demo_step():
+    pass
+    
