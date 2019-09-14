359e8b07eb8d202cde0e9e7294840b959d0439d5
Kenny Wong <wky123@hotmail.co.uk>
2019-09-14 22:20:34 +0100

    adapt transformer model

--- transformer.py
+++ transformer.py
@@ -1,12 +1,14 @@
 '''
 My own transformer implementation.
+This is for a *language model*, so we only have the decoder; there is no encoder.
 
 Main differences from the example implementation in the Tensorflow tutorial:
 - The positional embeddings are completely learnable.
   (This is likely to work well on short sequences, but perhaps less so on longer sequences.)
-- In the attention query/key vector depth and the number of attention heads can be chosen
-  independently of the overall model dimension.
-- The value vectors for attention are kept at the model dimension.
+- In the attention units, the query/key vector depth and the number of attention heads
+  can be chosen independently of the overall model dimension.
+- The value vectors have the same dimension as the model dimension; they are not mapped
+  to the dimensionality of the queries and keys.
 - Relu activations are applied wherever possible, including to the attention output,
   and to the feed-forward output.
 '''
@@ -21,9 +23,9 @@ class WordAndPositionalEmbedding(tf.keras.layers.Layer):
         self.model_dims = model_dims
     
     def build(self, input_shape):
-        sequence_len = input_shape[1]
+        seq_len = input_shape[1]
         self.positional_embedding = tf.Variable(
-                tf.random.uniform(minval=-0.05, maxval=0.05, shape=(sequence_len, self.model_dims)))
+                tf.random.uniform(minval=-0.05, maxval=0.05, shape=(seq_len, self.model_dims)))
         
     def call(self, inputs):
         word_embedding = self.word_embed_layer(inputs)
@@ -32,13 +34,13 @@ class WordAndPositionalEmbedding(tf.keras.layers.Layer):
 
 class AttentionHead(tf.keras.layers.Layer):
     
-    def __init__(self, model_dims, attention_depth, causal):
+    def __init__(self, model_dims, attention_depth):
         super(AttentionHead, self).__init__()
         self.sqrt_depth = tf.Variable(tf.math.sqrt(tf.cast(attention_depth, 'float32')),
                                       trainable=False)
         self.query_dense = tf.keras.layers.Dense(attention_depth)
         self.key_dense = tf.keras.layers.Dense(attention_depth)
-        self.attention_layer = tf.keras.layers.Attention(causal=causal)
+        self.attention_layer = tf.keras.layers.Attention(causal=True) # masking the future
         self.output_dense = tf.keras.layers.Dense(model_dims, activation='relu')
 
     def call(self, inputs):
@@ -52,19 +54,17 @@ class AttentionHead(tf.keras.layers.Layer):
 
 class MultiHeadAttentionUnit(tf.keras.layers.Layer):
     
-    def __init__(self, model_dims, attention_depth, causal, num_heads):
+    def __init__(self, model_dims, attention_depth, num_heads):
         super(MultiHeadAttentionUnit, self).__init__()
-        self.attention_heads = [AttentionHead(model_dims, attention_depth, causal)
+        self.attention_heads = [AttentionHead(model_dims, attention_depth)
                                 for _ in range(num_heads)]
         self.dropout = tf.keras.layers.Dropout(rate=0.2)
         self.norm = tf.keras.layers.LayerNormalization()
     
     def call(self, inputs, training):
-        query_inputs = inputs[0]
-        key_value_inputs = inputs[1]
-        attention_outputs = [head([query_inputs, key_value_inputs]) for head in self.attention_heads]
+        attention_outputs = [head([inputs, inputs]) for head in self.attention_heads]
         attention_output_sum = tf.add_n(attention_outputs)
-        return self.norm(query_inputs + self.dropout(attention_output_sum, training=training))
+        return self.norm(inputs + self.dropout(attention_output_sum, training=training))
 
 
 class FeedForwardUnit(tf.keras.layers.Layer):
@@ -80,111 +80,49 @@ class FeedForwardUnit(tf.keras.layers.Layer):
         hidden_values = self.hidden_dense(inputs)
         outputs = self.output_dense(hidden_values)
         return self.norm(inputs + self.dropout(outputs, training=training))
+    
 
-
-class EncoderLayer(tf.keras.layers.Layer):
+class TransformerLayer(tf.keras.layers.Layer):
     
     def __init__(self, model_dims, attention_depth, num_heads, hidden_dims):
-        super(EncoderLayer, self).__init__()
-        self.self_attention = MultiHeadAttentionUnit(model_dims, attention_depth, False, num_heads)
+        super(TransformerLayer, self).__init__()
+        self.self_attention = MultiHeadAttentionUnit(model_dims, attention_depth, num_heads)
         self.feed_forward = FeedForwardUnit(model_dims, hidden_dims)
     
     def call(self, inputs, training):
-        self_attention_output = self.self_attention([inputs, inputs], training=training)
-        return self.feed_forward(self_attention_output, training=training)
+        attention_output = self.self_attention(inputs, training=training)
+        return self.feed_forward(attention_output, training=training)
     
-
-class DecoderLayer(tf.keras.layers.Layer):
-    
-    def __init__(self, model_dims, attention_depth, num_heads, hidden_dims):
-        super(DecoderLayer, self).__init__()
-        self.self_attention = MultiHeadAttentionUnit(model_dims, attention_depth, True, num_heads)
-        self.cross_attention = MultiHeadAttentionUnit(model_dims, attention_depth, False, num_heads)
-        self.feed_forward = FeedForwardUnit(model_dims, hidden_dims)
-    
-    def call(self, inputs, training):
-        decoder_inputs = inputs[0]
-        encoder_outputs = inputs[1]
-        self_attention_output = self.self_attention([decoder_inputs, decoder_inputs],
-                                                    training=training)
-        cross_attention_output = self.cross_attention([self_attention_output, encoder_outputs],
-                                                      training=training)
-        return self.feed_forward(cross_attention_output, training=training)
-    
-
-class Encoder(tf.keras.models.Model):
-    
-    def __init__(self, vocab_size, num_layers, model_dims, attention_depth, num_heads, hidden_dims):
-        super(Encoder, self).__init__()
-        self.embedding_layer = WordAndPositionalEmbedding(model_dims, vocab_size)
-        self.transformer_layers = [EncoderLayer(model_dims, attention_depth, num_heads, hidden_dims)
-                                   for _ in range(num_layers)]
-    
-    def call(self, inputs, training):
-        values = self.embedding_layer(inputs)
-        for layer in self.transformer_layers:
-            values = layer(values, training=training)
-        return values
-
-
-class Decoder(tf.keras.models.Model):
-    
-    def __init__(self, vocab_size, num_layers, model_dims, attention_depth, num_heads, hidden_dims):
-        super(Decoder, self).__init__()
-        self.embedding_layer = WordAndPositionalEmbedding(model_dims, vocab_size)
-        self.transformer_layers = [DecoderLayer(model_dims, attention_depth, num_heads, hidden_dims)
-                                   for _ in range(num_layers)]
-    
-    def call(self, inputs, training):
-        decoder_inputs = inputs[0]
-        encoder_outputs = inputs[1]
-        decoder_values = self.embedding_layer(decoder_inputs)
-        for layer in self.transformer_layers:
-            decoder_values = layer([decoder_values, encoder_outputs], training=training)
-        return decoder_values
-
 
 class Transformer(tf.keras.models.Model):
-    '''Transformer model'''
+    '''Transformer language model'''
     
-    def __init__(self, encoder_vocab_size, decoder_vocab_size,
-                 num_targets=None, output_activation='softmax',
-                 encoder_num_layers=2, decoder_num_layers=2,
-                 model_dims=128, attention_depth=16, num_heads=4, hidden_dims=128):
+    def __init__(self, vocab_size, num_layers, model_dims, attention_depth,
+                 num_heads, hidden_dims):
         '''
-        :param encoder_vocab_size: number of distinct tokens in encoder input
-        :param decoder_vocab_size: number of distinct tokens in decoder input
-        :param num_targets: number of distinct target tokens (default=decoder_vocab_size)
-        :param output_activation: activation to apply to final output (default='softmax')
-        :param encoder_num_layers: number of transformer layers in encoder (default=2)
-        :param decoder_num_layers: number of transformer layers in decoder (default=2)
-        :param model_dims: the embedding size, also the output size of the transformer layers
-        :param attention_depth: the size of a query vector or key vector in an attention unit
-        :param num_heads: the number of attention heads in each each attention unit
-        :param hidden_dims: the size of the hidden layer in the feed-foward units
+        :param vocab_size: vocabulary size
+        :param num_layers: number of transformer layers
+        :param model_dims: embedding dimensionality, also dimensionality of
+                           output of transformer layers
+        :param attention_depth: sizes of attention queries and keys
+        :param num_heads: number of attention heads
+        :param hidden_dims: dimensionality of hidden layers in feedforward units
         '''
         super(Transformer, self).__init__()
-        self.encoder = Encoder(encoder_vocab_size, encoder_num_layers,
-                               model_dims, attention_depth, num_heads, hidden_dims)
-        self.decoder = Decoder(decoder_vocab_size, decoder_num_layers,
-                               model_dims, attention_depth, num_heads, hidden_dims)
-        if num_targets is None:
-            num_targets = decoder_vocab_size
-        self.final_layer = tf.keras.layers.Dense(num_targets, activation=output_activation)
+        self.embedding_layer = WordAndPositionalEmbedding(model_dims, vocab_size)
+        self.main_layers = [TransformerLayer(model_dims, attention_depth, num_heads, hidden_dims)
+                            for _ in range(num_layers)]
+        self.final_layer = tf.keras.layers.Dense(vocab_size, activation='softmax')
     
     def call(self, inputs, training):
         '''
-        :param inputs:
-            inputs[0]: encoder inputs, shape=(batch_size, encoder_sequence_len), dtype=int
-                                       values between 0 and (encoder_vocab_size - 1)
-            inputs[1]: decoder inputs, shape=(batch_size, decoder_sequence_len), dtype=int
-                                       values between 0 and (decoder_vocab_size - 1)
-        :param training: whether to use dropout in training mode or test mode
-        :returns: predictions, shape=(batch_size, dec_sequence_len, num_targets), dtype=float
-                  e.g. if output_activation='softmax' then these are the target probabilities
+        :param inputs: word sequence inputs
+                       shape=(batch_size, seq_len), dtype=int32, maxval=vocab_size
+        :param training: whether to run dropout in training mode
+        :returns: probabilities for next word
+                  shape=(batch_size, seq_len, vocab_size), dtype=float32
         '''
-        encoder_inputs = inputs[0]
-        decoder_inputs = inputs[1]
-        encoder_outputs = self.encoder(encoder_inputs, training=training)
-        decoder_outputs = self.decoder([decoder_inputs, encoder_outputs], training=training)
-        return self.final_layer(decoder_outputs)
+        values = self.embedding_layer(inputs)
+        for layer in self.main_layers:
+            values = layer(values, training=training)
+        return self.final_layer(values)
